67dd84b99f53de492f056ecea9cee52230f3e188
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-13 12:49:04 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by Romanov on 13.12.2021.
+//
+
+#ifndef BLOCKCHANIN_MAIN_H
+#define BLOCKCHANIN_MAIN_H
+
+#include <iostream>
+#include "include/TransactionData.h"
+#include "include/Block.h"
+#include "include/Blockchain.h"
+
+#endif //BLOCKCHANIN_MAIN_H
