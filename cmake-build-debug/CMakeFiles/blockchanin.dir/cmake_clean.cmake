34665187e7c45c0f2927d6cb9467db41516dfceb
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-13 13:48:42 +0300

    Create CMakeLists

---
+++ cmake-build-debug/CMakeFiles/blockchanin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blockchanin.dir/main.cpp.obj"
+  "CMakeFiles/blockchanin.dir/src/Block.cpp.obj"
+  "CMakeFiles/blockchanin.dir/src/Blockchain.cpp.obj"
+  "CMakeFiles/blockchanin.dir/src/TransactionData.cpp.obj"
+  "blockchanin.exe"
+  "blockchanin.exe.manifest"
+  "blockchanin.pdb"
+  "libblockchanin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blockchanin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
